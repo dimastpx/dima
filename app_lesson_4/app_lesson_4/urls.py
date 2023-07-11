acecbfb2a1051edb5a34726c35a9be0ffc934ba7
dima <dimas30072007@gmail.com>
2023-07-11 12:39:20 +1000

    ааа

--- app_lesson_4/app_lesson_4/urls.py
+++ app_lesson_4/app_lesson_4/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 
 from django.urls import path
 from app_lesson_4.views import lesson_4_view
 
 urlpatterns = [
-    # ... другие URL-шаблоны вашего проекта ...
-    path('lesson_4/', lesson_4_view),
+    path('lesson_4/', lesson_4_view, name='lesson_4_view'),
 ]
